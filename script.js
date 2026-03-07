8473d009657901c4a95b14a863f618e41b8aa9d8
rakibul258862-crypto <rakibul258862@gmail.com>
2026-03-07 14:53:25 +0600

    main page nav bar is completed

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log('welcome to sing in page ')
+
+document.getElementById('singInBtn').addEventListener('click',() => {
+    // step-1 get the  username input and value 
+    const userNameInput = document.getElementById("Username-Input");
+    const usernameValue =userNameInput.value;
+    // console.log(usernameValue);
+    // step-2 get the password and value 
+    const passwordInput =document.getElementById("password-input");
+    const passwordValue =passwordInput.value;
+    // console.log(passwordValue);
+    // step-3 username input value == get password input value match karbo 
+    // if match === false ==> alert ==>'Invalid your information' and if match === true ==> 'loading main section' 
+    if(usernameValue === "admin" && passwordValue ===  'admin123'){
+        alert('Sing In successful');
+        window.location.assign('/main.html');
+    }else{
+        alert('sing in failed');
+    }
+
+
+})
