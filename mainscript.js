cbf482ceeeee78ed6f73474df6a55d832bf2e27a
rakibul258862-crypto <rakibul258862@gmail.com>
2026-03-08 12:52:50 +0600

    add card a top color border

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,4 @@
-// all issued 
+// all issued fetch and json konrchi
 const loadAllIssues =()=>{
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res => res.json())
@@ -11,6 +11,7 @@ const issuedContainer =document.getElementById('issues-container');
 issuedContainer.innerHTML = ''; 
 //2 loop and cerated div element and add class list  
 issues.forEach(issue => {
+    // damo data api 
 //     "id": 1,
 // "title": "Fix navigation menu on mobile devices",
 // "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
@@ -19,14 +20,23 @@ issues.forEach(issue => {
 // "bug",
 // "help wanted"
 // ],
+
 // "priority": "high",
 // "author": "john_doe",
 // "assignee": "jane_smith",
 // "createdAt": "2024-01-15T10:30:00Z",
 // "updatedAt": "2024-01-15T10:30:00Z"
 // },
+            //border color add condition status == --> open hoi ==>top border color change[#00A96e]
+            // and status ==-->close hoi ==> top border color [#A855F7] 
+    let topColorBorder ;
+    if(issue.status === "open"){
+        topColorBorder ="border-t-4 border-[#00A96e]"
+    }else if(issue.status === "closed"){
+        topColorBorder = "border-t-4 border-[#A855F7]"
+    };
     const card = document.createElement("div");
-    card.classList= 'card bg-base-100 w-auto shadow-md';
+    card.classList= `card bg-base-100 w-auto shadow-md ${topColorBorder}`;
     // priority ar color change condition 
     let priorityColor;
     if(issue.priority == "high"){
@@ -36,6 +46,7 @@ issues.forEach(issue => {
     }else if(issue.priority == "low"){
         priorityColor ="badge badge-ghost";
     }
+    // card ar innhtml created
     card.innerHTML=`
          <div class="card-body">
     <div class="top-icons flex justify-between">
@@ -64,7 +75,6 @@ issues.forEach(issue => {
       </div>
       </div>
       </div>
-
     `;
     // 3 append in container
     issuedContainer.appendChild(card);
